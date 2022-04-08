64cd7e312e309bd95b6c869799274871f165135b
Selman Koral <selmankorall@gmail.com>
2022-04-08 16:31:20 +0300

    Create 220407_send_emails.py

---
+++ 220407_send_emails/220407_send_emails.py
@@ -0,0 +1,11 @@
+# Allow less secure apps first : https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4Mm9e0_M6ijQ14K5o5KnPjQ6D9CTUHqnt-wZaigWyKVGOyxYPIqsGtRPL5Ye7frM6zE3dnWBxENudHWLqCskPLFnWCWwQ 
+import smtplib
+password = 
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.login("selmankorall", password)
+message = "\n This mail is sent via vscode. Hope you are doing fine. \n Sincerely,\nKoral"
+mails = ["selmankorall@gmail.com"]
+server.sendmail("selmankorall@gmail.com", mails, message)
+server.quit()
