9ecbdc0c80f5896e03231734582ebb62d7611598
Selman Koral <selmankorall@gmail.com>
2022-04-15 04:06:18 +0300

    Update 220407_send_emails.py

--- 220407_send_emails/220407_send_emails.py
+++ 220407_send_emails/220407_send_emails.py
@@ -1,6 +1,6 @@
 # Allow less secure apps first : https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4Mm9e0_M6ijQ14K5o5KnPjQ6D9CTUHqnt-wZaigWyKVGOyxYPIqsGtRPL5Ye7frM6zE3dnWBxENudHWLqCskPLFnWCWwQ 
 import smtplib
-password = 
+password = "yourpasswordhere"
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
 
